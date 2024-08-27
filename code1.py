4f63a2a92f9a08508eee406e25ed8e01d539c035
A24bhi <102672944+A24bhi@users.noreply.github.com>
2024-08-27 12:40:50 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,7 +1,5 @@
 print('Printing numbers which are divisible by 7 but not by 5 in between 2000 and 3200')
 numbers = []
-numbers = []
-numbers = []
 for i in range(2000, 3201):
     if i % 7 == 0 and i% 5 != 0:
          numbers.append(str(i))
